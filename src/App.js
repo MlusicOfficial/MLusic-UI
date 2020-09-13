2d20860be2aea631cce1d44d6a71c32c0d6337b2
Andrew McDonald <andrewemcdonald@outlook.com>
2020-09-13 13:25:40 -0700

    Add test message.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from '@blueprintjs/core';
+import { Callout } from '@blueprintjs/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button></Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Andrew was here!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Callout title='Test!' intent='danger'>This is a test site. Andrew.</Callout>
   );
 }
 
